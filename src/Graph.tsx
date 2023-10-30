bf57392034d5576d4acdbcdd486e5335b4220091
codenamerey <100455152+codenamerey@users.noreply.github.com>
2023-10-30 09:05:44 +0800

    Show graph

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -34,6 +34,20 @@ class Graph extends Component<IProps, {}> {
     // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as PerspectiveViewerElement;
 
+    elem.setAttribute("view", "y_line");
+    elem.setAttribute("column-pivots", "['stock']");
+    elem.setAttribute("row-pivots", "['timestamp']");
+    elem.setAttribute("columns", "['top_ask_price']");
+    elem.setAttribute("aggregates",
+    `
+      {
+        "stock": "distinct count",
+        "top_bid_price": "avg",
+        "top_ask_price": "avg",
+        "timestamp": "distinct count"
+      }
+    `);
+
     const schema = {
       stock: 'string',
       top_ask_price: 'float',
